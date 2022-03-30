7002d7db2436d15687c71858b7ee7f56a2f23136
Jack <Jmstrope24@students.bmchs.org>
2022-03-30 15:32:52 -0500

    ADDED

--- Unit3_Project.py
+++ Unit3_Project.py
@@ -1,4 +1,6 @@
+
 import random
+
 
 # Global Variables
 MONTHS = ['Dummy','January','February','March','April','May','June','July',
@@ -12,6 +14,34 @@ current_month = 3
 
 # all function definitions
 
+def Quit():
+    global playing
+    Game_Over_Choice = input("Would you like to quit the game? All progress will be lost")
+    if Game_Over_Choice == "Yes" or Game_Over_Choice == "yes":
+        playing = False
+    else:
+        print("Continuing Game.")
+
+
+def Hunt():
+    global food
+    food += 100
+    days_this_hunt = random.randint(2, 5) 
+    print(f"You hunted for {days_this_hunt} miles")
+    add_day(days_this_hunt)
+
+
+def Rest():
+    global health
+    health_added = 1
+    health += health_added
+    days_this_travel = random.randint(2,5)
+    print(f"You rest for {days_this_travel} days \n"
+    "and gained 1 extra health point")
+
+
+    add_day(days_this_travel)
+
 
 def Travel():
     global miles_to_go
@@ -21,28 +51,42 @@ def Travel():
     days_this_travel = random.randint(3, 7) 
     print(f"You Traveled {miles_traveled} miles")
 
-    add_day(days_this_travel)
+    add_day(days_this_travel) 
 
 def Status():
-    global food
     print()
     print(f"You have {miles_to_go} miles left to travel")
     print()
     print(f"You have {food} pounds of food left" )
     print()
+    print(f"You have {health} health left")
+    print()
     print(f"It is {MONTHS[current_month]} {current_day}")
     print()
+
 def add_day(days):
-    global current_day, current_month
+    global current_day, current_month, playing, health
     current_day += days
     if MONTHS[current_month] in MONTHS_WITH_30_DAYS:
         if current_day > 30:
             current_day -= 30 
             current_month += 1
+            health -= 2
     else:
         if current_day > 31:
             current_day -= 31
             current_month += 1
+            health -= 2
+        if current_month == 13:
+            print('you are outta time')
+            playing = False
+
+    consume_food(days)
+
+def consume_food(days):
+    global food
+
+    food -= 5 * days
 
 def Help():
     print("The possible inputs are: Hunting, Traveling, Resting, Player Status, and Quit ")
@@ -66,22 +110,57 @@ def Help():
             print()
 
 
+
+
+
 print("Welcome to the oregon Trail. \n"
     "Your goal is to reach Oregon from Independence, Missouri by December 31st. \n"
     "The current day is March 1st \n"
+    "You will lose the game if you're health reaches 0\n"
+    "Or if you run out of food completely \n"
+    "So be sure to rest and hunt regularly \n"
     "Good luck on your adventure. \n"
-    "Input Help if you need help at anytime! \n")
+    "Input 'Help' if you need help at anytime! \n")
+Name = input("What is your name, traveler? > ")
 
-while True:
+playing = True
+while playing:
     Player_Action = input("What would you like to do? (Hunt, Travel, Rest, Status, Help, and Quit) > ")
-    if Player_Action == "Travel":
+    if Player_Action == "Travel" or Player_Action == "travel":
         Travel()
 
-    elif Player_Action == "Status":
+    elif Player_Action == "Status" or Player_Action == "status":
         Status()
 
-    elif Player_Action == "Help":
+    elif Player_Action == "Help" or Player_Action == "help":
         Help()
+    
+    elif Player_Action == "Rest" or Player_Action == "rest":
+        Rest()
+    
+    elif Player_Action == "Hunt" or Player_Action == "hunt":
+        Hunt()
+
+    elif Player_Action == "Quit" or Player_Action == "quit":
+        Quit()
+
+    if health == 0:
+        print("You have ran out of health and died on the Oregon trail.")
+        print("Better Luck Next Time")
+        playing = False
+    
+    if food == 0:
+        print("You ran out of food, and starved.")
+        print("Better Luck Next Time")
+        playing = False
+    
+    if miles_to_go == 0:
+        print("You made it to Oregon before Winter.")
+        print("Congratulations, you survived the arduos Oregon Trail")
+        playing = False
+
+
+
 
 
     
